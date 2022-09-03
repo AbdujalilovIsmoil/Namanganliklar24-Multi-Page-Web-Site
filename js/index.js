b4db3e8b7c0c42131240cc9c3ef59fa2843fb5c0
= <ismoilbek20050421@gmail.com>
2022-09-03 11:12:07 +0500

    Namanganliklar24

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 "use strict";
 
 this.addEventListener("DOMContentLoaded", () => {
+  // Modal
   let modal = document.querySelector(".modal");
   let modalCloseBtn = document.querySelector("[data-close]");
   let modalOpenBtn = document.querySelector("[data-open]");
@@ -40,11 +41,43 @@ this.addEventListener("DOMContentLoaded", () => {
     }
   });
 
+  // Cursor
+
   let cursor = document.querySelector("#cursor");
   this.addEventListener("mousemove", (e) => {
     let x = e.clientX;
     let y = e.clientY;
     cursor.style.left = x + "px";
     cursor.style.top = y + "px";
+  });
+
+  // Navigation
+  let navigation = document.querySelector(".navigation");
+  let faBars = document.querySelector(".fa-bars");
+  let faTimes = document.querySelector(".navigation .fa-times");
+  let navigationLinks = document.querySelectorAll(".navgation__list_item-link");
+
+  faBars.addEventListener("click", () => {
+    navigation.classList.add("active");
+    body.style.overflow = "hidden";
+  });
+
+  faTimes.addEventListener("click", () => {
+    navigation.classList.remove("active");
+    body.style.overflow = "";
+  });
+
+  for (let navigationLink of navigationLinks) {
+    navigationLink.addEventListener("click", () => {
+      navigation.classList.remove("active");
+      body.style.overflow = "";
+    });
+  }
+
+  this.addEventListener("keydown", (event) => {
+    if (event.code == "Escape") {
+      navigation.classList.remove("active");
+      body.style.overflow = "";
+    }
   });
 });
